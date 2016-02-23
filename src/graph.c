220fb2833466c97afc53755ef8aebc4d66b391a1
Benjamin James <bjames@openmailbox.org>
2016-02-23 17:52:49 -0600

    redid the whole thing
    l0ck SBIRS ANDVT Watch USCODE PLO import chaining bomb domestic
    disruption arrangements CBM Swine CID Flashbangs

--- src/graph.c
+++ src/graph.c
@@ -2,85 +2,90 @@
 #include "config.h"
 #endif
 
-#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "graph.h"
 
-int graph_init(struct graph *g, size_t alloc, int (*data_cmp)(const __DATA_T left, const __DATA_T right), int (*weight_cmp)(const __WEIGHT_T left, const __WEIGHT_T right), uintptr_t (*hash)(const __DATA_T data))
+int graph_init(struct graph *g, int (*data_cmp)(const DATA_T left, const DATA_T right), int (*weight_cmp)(const WEIGHT_T left, const WEIGHT_T right))
 {
-	g->vertices = calloc(alloc, sizeof *g->vertices);
-	if (!g->vertices) {
-		perror("calloc");
+	if (g == NULL) {
 		return -1;
 	}
-	g->size = alloc;
+	g->head = NULL;
 	g->data_cmp = data_cmp;
 	g->weight_cmp = weight_cmp;
-	g->hash = hash;
 	return 0;
 }
 
 int graph_delete(struct graph *g)
 {
-	uintptr_t i;
-	for (i = 0; i < g->size; i++) {
-		if (!g->vertices[i].is_set) {
-			continue;
-		}
-		graph_remove_vertex(g->vertices + i);
+	struct vertex *v = g->head;
+	for (v = g->head; v != NULL; v = v->next) {
+		graph_remove_vertex(g, v);
 	}
-	free(g->vertices);
 	return 0;
 }
 
-
-int graph_get_vertex(struct graph *g, const __DATA_T data, struct vertex **v_ret)
+struct vertex * graph_get_vertex(struct graph *g, const DATA_T data)
 {
-	uintptr_t i;
-	for (i = g->hash(data) % g->size; i < g->size; i++) {
-		if (!g->vertices[i].is_set) {
-			break;
+	struct vertex *v = g->head;
+	for (v = g->head; v != NULL; v = v->next) {
+		if (!g->data_cmp(data, v->data)) {
+			return v;
 		}
-		if (!g->data_cmp(g->vertices[i].data, data)) {
-			*v_ret = g->vertices + i;
-			return 0;
-		}
-	}
-	return -1;
-}
-int graph_get_edge(struct vertex *from, struct vertex *to, struct edge **e_ret)
-{
-	struct edge *e;
-	for (e = from->list; e != NULL; e = e->next) {
-		if (e->to == to) {
-			*e_ret = e;
-			return 0;
-		}
-	}
-	return -1;
-}
-
-struct vertex * graph_add_vertex(struct graph *g, const __DATA_T data)
-{
-	uintptr_t i;
-	for (i = g->hash(data) % g->size; i < g->size; i++) {
-		if (g->vertices[i].is_set) {
-			if (!g->data_cmp(g->vertices[i].data, data)) {
-				return NULL;
-			}
-			continue;
-		}
-		g->vertices[i].is_set = 1;
-		g->vertices[i].list = NULL;
-		g->vertices[i].data = data;
-		return g->vertices + i;
 	}
 	return NULL;
 }
 
-static struct edge * edge_new(const __WEIGHT_T weight, struct vertex *to, struct edge *next)
+struct edge * graph_get_edge(struct vertex *from, struct vertex *to)
+{
+	struct edge *e = from->edges;
+	for (e = from->edges; e != NULL; e = e->next) {
+		if (e->to == to) {
+			return e;
+		}
+	}
+	return NULL;
+}
+
+struct vertex * graph_add_vertex(struct graph *g, const DATA_T data)
+{
+	struct vertex *v, *temp, *n;
+	for (temp = NULL, v = g->head; v != NULL; temp = v, v = v->next) {
+		if (!g->data_cmp(data, v->data)) {
+			return v;
+		}
+	}
+	n = malloc(sizeof *n);
+	if (n == NULL) {
+		perror("malloc");
+		return NULL;
+	} else if (temp == NULL) {
+		g->head = n;
+	} else {
+		temp->next = n;
+	}
+	n->data = data;
+	n->next = NULL;
+	return n;
+}
+
+static void _add_edge(struct edge **list, struct edge *n, int (*weight_cmp)(const WEIGHT_T left, const WEIGHT_T right))
+{
+	struct edge *tmp = *list, *prev = NULL;
+	while (tmp != NULL && weight_cmp(tmp->weight, n->weight) <= 0) {		prev = tmp;
+		tmp = tmp->next;
+	}
+	n->next = tmp;
+	if (prev == NULL) {
+		*list = n;
+	} else {
+		prev->next = n;
+	}
+}
+
+struct edge * graph_add_edge(struct graph *g, struct vertex *from, struct vertex *to, const WEIGHT_T weight)
 {
 	struct edge *e = malloc(sizeof *e);
 	if (!e) {
@@ -89,42 +94,73 @@ static struct edge * edge_new(const __WEIGHT_T weight, struct vertex *to, struct
 	}
 	e->weight = weight;
 	e->to = to;
-	e->next = next;
+	e->next = NULL;
+	_add_edge(&from->edges, e, g->weight_cmp);
 	return e;
 }
-struct edge * graph_add_edge(struct graph *g, struct vertex *from, struct vertex *to, const __WEIGHT_T weight)
+
+int graph_remove_vertex_deep(struct graph *g, struct vertex *v)
 {
-	struct edge *e, *prev;
-	if (from->list && to == from->list->to) {
-		return NULL;
-	} else if (!from->list || g->weight_cmp(weight, from->list->weight) >= 0) {
-		return (from->list = edge_new(weight, to, from->list));
+	struct vertex *t;
+	for (t = g->head; t != NULL; t = t->next) {
+		graph_remove_edge(t, v);
 	}
-	for (prev = from->list, e = prev->next; prev != NULL; prev = e, e = e->next) {
-		if (e && to == e->to) {
-			return NULL;
-		}
-		if (e && g->weight_cmp(weight, e->weight) < 0) {
-			continue;
-		}
-		return (prev->next = edge_new(weight, to, e));
-	}
-	return NULL;
+	return graph_remove_vertex(g, v);
 }
 
-int graph_remove_vertex(struct vertex *v)
+int graph_remove_vertex(struct graph *g, struct vertex *v)
 {
-	struct edge *temp;
-	while ((temp = v->list) != NULL) {
-		v->list = v->list->next;
-		graph_remove_edge(temp);
+	struct edge *e;
+	if (g->head == NULL) {
+		return 0;
 	}
-	v->is_set = 0;
+	while (v->edges != NULL) {
+		e = v->edges;
+		v->edges = v->edges->next;
+		free(e);
+	}
+	if (g->head == v) {
+		struct vertex *temp = g->head;
+		g->head = g->head->next;
+		free(temp);
+	} else {
+		struct vertex *cur = g->head->next;
+		struct vertex *prev = g->head;
+		while (cur != NULL && prev != NULL) {
+			if (v == cur) {
+				struct vertex *tmp = cur;
+				prev->next = cur->next;
+				free(tmp);
+				break;
+			}
+			prev = cur;
+			cur = cur->next;
+		}
+	}
 	return 0;
 }
 
-int graph_remove_edge(struct edge *e)
+int graph_remove_edge(struct vertex *from, struct vertex *to)
 {
-	free(e);
+	if (from == NULL || from->edges == NULL) {
+		return 0;
+	} else if (from->edges->to == to) {
+		struct edge *tmp = from->edges;
+		from->edges = from->edges->next;
+		free(tmp);
+	} else {
+		struct edge *cur = from->edges->next;
+		struct edge *prev = from->edges;
+		while (cur != NULL && prev != NULL) {
+			if (to == cur->to) {
+				struct edge *tmp = cur;
+				prev->next = cur->next;
+				free(tmp);
+				break;
+			}
+			prev = cur;
+			cur = cur->next;
+		}
+	}
 	return 0;
 }
--- src/graph.h
+++ src/graph.h
@@ -1,49 +1,43 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h> /* size_t */
-
-
-#ifndef __WEIGHT_T
-#define __WEIGHT_T double
+#ifndef WEIGHT_T
+#define WEIGHT_T int
 #endif
 
-#ifndef __DATA_T
-#define __DATA_T long
+#ifndef DATA_T
+#define DATA_T char
 #endif
 
 struct edge {
-	__WEIGHT_T weight;
+	WEIGHT_T weight;
 	struct vertex *to;
 	struct edge *next;
 };
 
 struct vertex {
-	__DATA_T data;
-	bool is_set;
-	struct edge *list;
+	DATA_T data;
+	struct edge *edges;
+	struct vertex *next;
 };
 
 struct graph {
-	struct vertex *vertices;
-	size_t size;
-	int (*data_cmp)(const __DATA_T left, const __DATA_T right);
-	int (*weight_cmp)(const __WEIGHT_T left, const __WEIGHT_T right);
-	uintptr_t (*hash)(const __DATA_T data);
+	struct vertex *head;
+	int (*data_cmp)(const DATA_T left, const DATA_T right);
+	int (*weight_cmp)(const WEIGHT_T left, const WEIGHT_T right);
 };
 
-int graph_init(struct graph *g, size_t alloc, int (*data_cmp)(const __DATA_T left, const __DATA_T right), int (*weight_cmp)(const __WEIGHT_T left, const __WEIGHT_T right), uintptr_t (*hash)(const __DATA_T data));
+int graph_init(struct graph *g, int (*data_cmp)(const DATA_T left, const DATA_T right), int (*weight_cmp)(const WEIGHT_T left, const WEIGHT_T right));
 int graph_delete(struct graph *g);
 
-int graph_get_vertex(struct graph *g, const __DATA_T data, struct vertex **v_ret);
-int graph_get_edge(struct vertex *from, struct vertex *to, struct edge **e_ret);
+struct vertex * graph_get_vertex(struct graph *g, const DATA_T data);
+struct edge * graph_get_edge(struct vertex *from, struct vertex *to);
 
-struct vertex *graph_add_vertex(struct graph *g, const __DATA_T data);
-struct edge *graph_add_edge(struct graph *g, struct vertex *from, struct vertex *to, const __WEIGHT_T weight);
+struct vertex * graph_add_vertex(struct graph *g, const DATA_T data);
+struct edge * graph_add_edge(struct graph *g, struct vertex *from, struct vertex *to, const WEIGHT_T weight);
 
-int graph_remove_vertex(struct vertex *v);
-int graph_remove_edge(struct edge *e);
+int graph_remove_vertex(struct graph *g, struct vertex *v);
+int graph_remove_vertex_deep(struct graph *g, struct vertex *);
+int graph_remove_edge(struct vertex *from, struct vertex *to);
 
 #endif
